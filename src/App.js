0de8c7a408c7902e03cc4afa67579b6ec5e692ca
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-09-16 11:39:32 +0300

    ported the mauerwerk demo to one based on the latest react.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
+
+// ray test touch <
 import React from 'react';
-import logo from './logo.svg';
+
+import MauerwerkAdpativeList from './components/MauerwerkAdpativeList';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MauerwerkAdpativeList />
     </div>
   );
 }
 
 export default App;
+// ray test touch >
